44d4390b44693b81b62f0ea15a7769ad5fdbef90
Brian Winkers <bwinkers@gmail.com>
2014-10-11 15:14:50 -0500

    prebuild

--- activerules-bootstrap.sh
+++ activerules-bootstrap.sh
@@ -12,26 +12,40 @@ then
 
     # Update and begin installing some utility tools
     apt-get -y update
+    apt-get -y dist-upgrade
     apt-get install -y python-software-properties
     apt-get install -y vim git subversion curl
     apt-get install -y memcached build-essential
 
-    # Add nodejs repo
-    a -y ppa:chris-lea/node.js
-    apt-get -y update
-
-    # Install nodejs
-    apt-get install -y nodejs
-
     # Install latest stable version of MongoDB
     apt-get install -y mongodb-10gen
+
+    # Add nodejs repo
+    curl -sL https://deb.nodesource.com/setup | bash -
+
+    # Install nodejs and npm 
+    apt-get install -y nodejs 
+
+    # Install Grunt command line (cli)
+    npm install -g grunt-cli
+
+    # Change to the webroot and install the application dependencies
+    npm install --prefix /vagrant/www/default /vagrant/www/default
+    #(cd /vagrant/www/default && npm install)
+
+    # Install the 
+    #(cd /vag/www/default && grunt init:dev)
+    grunt --base /vagrant/www/default --gruntfile /vagrant/www/default/Gruntfile.js init:dev
+
+    # Run a little cleanup
+    apt-get autoremove
 
     # Symlink our host www to the guest /var/www folder
     ln -s /vagrant/www /var/www
 
     # Victory!
-    echo "You're all done! Your default node server should start listening on http://192.168.33.44/. For code, see: ar-vagrant-ubuntu14-node/www/default/server.js."
+    echo "VM created successfully! You can start your default node server listening on http://192.168.33.44:3300/. For code, see: ar-vagrant-ubuntu14-node/www/default/server.js."
 
     # Run it
-    nodejs /var/www/default/server.js
+    grunt --base /vagrant/www/default --gruntfile /vagrant/www/default/Gruntfile.js server
 fi
--- www/default/server.js
+++ www/default/server.js
@@ -1,20 +1,46 @@
-var http = require('http')
-  , sys  = require('sys');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    routes = require('./app/routes'),
+    exphbs = require('express3-handlebars'),
+    mongoose = require('mongoose'),
+    seeder = require('./app/seeder'),
+    app = express();
 
-var header = {
-	"Content-Type": "text/html"
-};
+app.set('port', process.env.PORT || 3300);
+app.set('views', __dirname + '/views');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts'
+}));
+app.set('view engine', 'handlebars');
 
-// Handle incoming requests
-var app = http.createServer( function( request, response ) {
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('some-secret-value-here'));
+app.use(app.router);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-	sys.puts("Incoming request! Woo!");
-	response.writeHeader(200, header);
-	response.write("<h2>Hello, ActiveRulers!</h2>");
-	response.end();
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
 
+//connect to the db server:
+mongoose.connect('mongodb://localhost/MyApp');
+mongoose.connection.on('open', function() {
+    console.log("Connected to Mongoose...");
+
+    // check if the db is empty, if so seed it with some contacts:
+    seeder.check();
 });
 
-// Listen for incoming requests on port 80
-app.listen(80);
+//routes list:
+routes.initialize(app);
 
+//finally boot up the server:
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
