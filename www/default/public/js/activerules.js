945ca2c1769f0be49d3fa09a1d7905b52b220759
Brian Winkers <bwinkers@gmail.com>
2014-10-25 20:08:17 -0500

    Various tweaks

--- www/default/public/js/activerules.js
+++ www/default/public/js/activerules.js
@@ -15152,7 +15152,7 @@ var global=typeof self !== "undefined" ? self : typeof window !== "undefined" ? 
 
 },{}],"underscore":[function(require,module,exports){
 module.exports=require('rI363Q');
-},{}]},{},["1JNp4M","S50dny","sBw5F0","rI363Q"])
+},{}]},{},["rI363Q","1JNp4M","sBw5F0","S50dny"])
 ;
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Marionette = require('backbone.marionette'),
